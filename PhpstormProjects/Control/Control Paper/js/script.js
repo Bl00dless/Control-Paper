010e227d3959c550c05a1ba2e8fb79184e27f669
Darnier_Heros <sasha.bryko@gmail.com>
2015-04-05 19:18:27 +0300

    1111

--- PhpstormProjects/Control/Control Paper/js/script.js
+++ PhpstormProjects/Control/Control Paper/js/script.js
@@ -2,5 +2,17 @@
  * Created by r2d2 on 03.04.15.
  */
 $(document).ready(function() {
+    var menuflag =0;
+    $('.menubutt').click(function() {
+        if (menuflag == 0) {
+            $('.menu').slideDown();
+            menuflag = 1;
+        }
+        else
+        {
+            $('.menu').slideUp();
+            menuflag=0;
+        }
 
+    })
 });
