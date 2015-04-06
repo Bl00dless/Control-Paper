b52070ce48c1fa1ca3dd573085cbf088b663255b
Darnier_Heros <sasha.bryko@gmail.com>
2015-04-06 18:32:34 +0300

    123123123

--- PhpstormProjects/Control/Control Paper/js/script.js
+++ PhpstormProjects/Control/Control Paper/js/script.js
@@ -28,13 +28,21 @@ $(document).ready(function() {
         }
     );
 
-    //slider
+    //sliders
     $('#slider').rhinoslider({
         controlsMousewheel: false,
         controlsPlayPause: false,
         showBullets: 'always',
         changeBullets: 'before',
         showControls: 'always'
+    });
+
+    $('#slider2').rhinoslider({
+        controlsMousewheel: false,
+        controlsPlayPause: false,
+        showBullets: 'always',
+        changeBullets: 'before',
+        showControls: 'never'
     });
 
 
@@ -44,4 +52,130 @@ $(document).ready(function() {
             after: "a.readmore"
         });
     });
+
+    //datepicker
+    $(function() {
+        $( "#datepicker" ).datepicker();
+    });
+
+
+    //tabs
+    $( "#tabs" ).tabs();
+
+
+
+
+    //validation
+    $('#login-password').focus(function(){
+        $(this).val('');
+    }).blur(function(){
+        if ($(this).val().length<=5){
+            $(this).css({border: '1px solid red'});
+        }
+        else
+        {
+            $(this).css({border: '1px solid green'});
+        }
+    });
+
+    var error = true;
+    var entry = $('.required-entry');
+    $(entry).focus(function(){
+        $(this).val('');
+        $(this).css("color","#000");
+    });
+    entry.blur(function(){
+        if ($(this).val() != ""){
+            $(this).css({border: '1px solid green'});
+            error = false;
+        }
+        else
+        {
+            $(this).css({border: '1px solid red'});
+            error = true;
+            $(this).tooltip({
+                items: "[title]",
+                content: function() {
+                    var element = $( entry );
+                    if ( element.is( "[title]" ) ) {
+                        return element.attr( "title" );
+                    }
+                },
+                position: {
+                    my: "center bottom-20",
+                    at: "center top",
+                    using: function( position, feedback ) {
+                        $( this ).css( position );
+                        $( "<div>" )
+                            .addClass( "tool-tip-show" )
+                            .appendTo( this );
+                    }
+                }
+            });
+        }
+    });
+
+    var login = $('.validate-email');
+    $(login).focus(function(){
+        $(this).val('');
+        $(this).css("color","#000");
+    });
+    login.blur(function () {
+        login = $(this);
+        var val = login.val();
+        if (validateEmail(val) == true){
+            $(login).css({border: '1px solid green'});
+            error = false;
+        }
+        else{
+            $(login).css({border: '1px solid red'});
+            error = true;
+            $(this).tooltip({
+                items: "[title]",
+                content: function() {
+                    var element = $( login );
+                    if ( element.is( "[title]" ) ) {
+                        return element.attr( "title" );
+                    }
+                },
+                position: {
+                    my: "center bottom-20",
+                    at: "center top",
+                    using: function( position, feedback ) {
+                        $( this ).css( position );
+                        $( "<div>" )
+                            .addClass( "tool-tip-show" )
+                            .appendTo( this );
+                    }
+                }
+            });
+        }
+    });
+
+
+    //------
+    $('#registration-submit').click(function(){
+        if (error != true){
+            alert( "Login correct!" );
+        }
+    });
+
+    $('spam-sign').click(function(){
+        if (error != true){
+            alert( "You joined Newsletter!!" );
+        }
+    });
+
+    $('#question-send').click(function(){
+        if (error != true){
+            alert( "We soon will have your question!!!" );
+        }
+    });
 });
+
+function validateEmail(elementValue){
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue);
+}
+
+
